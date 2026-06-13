9d6e7048a15932c0ead2e6d2d3517b00fa286b58
Jimi421 <b.e.bailey81@gmail.com>
2026-06-12 20:30:18 -0400

    added route to main.jsx for commandcalc

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
 import PivotCalc from './PivotCalc.jsx'
+import CommandCalc from './CommandCalc.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/pivot" element={<PivotCalc />} />
+        <Route path="/commandcalc" element={<CommandCalc />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
